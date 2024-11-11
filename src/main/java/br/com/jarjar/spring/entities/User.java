8603ae3abb21144db66101308286a6931b2e7694
matt <mattmoura.jvm@gmail.com>
2024-11-11 18:26:03 -0300

    mapping User and configuring H2db

--- src/main/java/br/com/jarjar/spring/entities/User.java
+++ src/main/java/br/com/jarjar/spring/entities/User.java
@@ -1,17 +1,28 @@
 package br.com.jarjar.spring.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
